f087a579a41188f40bd5fdb088c4dacb54707ba4
Martin Mädler <martin.maedler@gmail.com>
2023-01-06 10:59:04 +0100

    Implement HandleWinEvent
    And fix prefix for some global variables

--- WinTerminalTest.cpp
+++ WinTerminalTest.cpp
@@ -9,34 +9,93 @@ const SHORT CONSOLE_HEIGHT = 25;
 const SHORT CONSOLE_WIDTH = 80;
 
 // Global variables.
-HANDLE hInputConsole = NULL;
-HANDLE hOutputConsole = NULL;
+HANDLE g_hInputConsole = NULL;
+HANDLE g_hOutputConsole = NULL;
 HWINEVENTHOOK g_hook;
 
 void CALLBACK HandleWinEvent(HWINEVENTHOOK hook, DWORD event, HWND hwnd, LONG idObject, LONG idChild, DWORD dwEventThread, DWORD dwmsEventTime)
 {
     switch (event) {
     case EVENT_CONSOLE_CARET:
-        OutputDebugString(_T("EVENT_CONSOLE_CARET\n"));
+    {
+        TCHAR output[1024];
+        std::basic_string<TCHAR> flags;
+
+        COORD coord;
+        coord.X = LOWORD(idChild);
+        coord.Y = HIWORD(idChild);
+
+        if (idObject & CONSOLE_CARET_SELECTION) {
+            flags += _T("CONSOLE_CARET_SELECTION");
+        }
+        if (idObject & CONSOLE_CARET_VISIBLE) {
+            if (flags.length() > 0) {
+                flags += _T(", CONSOLE_CARET_VISIBLE");
+            }
+            else {
+                flags += _T("CONSOLE_CARET_VISIBLE");
+            }
+        }
+
+        _stprintf_s(output, 1024, _T("EVENT_CONSOLE_CARET: [X: %d, Y: %d, Flags: %s]\n"), coord.X, coord.Y, flags.c_str());
+        OutputDebugString(output);
         break;
+    }
     case EVENT_CONSOLE_UPDATE_REGION:
-        OutputDebugString(_T("EVENT_CONSOLE_UPDATE_REGION\n"));
+    {
+        TCHAR output[1024];
+
+        COORD coordStart;
+        coordStart.X = LOWORD(idObject);
+        coordStart.Y = HIWORD(idObject);
+
+        COORD coordEnd;
+        coordEnd.X = LOWORD(idChild);
+        coordEnd.Y = HIWORD(idChild);
+
+        _stprintf_s(output, 1024, _T("EVENT_CONSOLE_UPDATE_REGION: [Left: %d, Top: %d, Right: %d, Bottom: %d]\n"), coordStart.X, coordStart.Y, coordStart.X, coordEnd.Y);
+        OutputDebugString(output);
         break;
+    }
     case EVENT_CONSOLE_UPDATE_SIMPLE:
-        OutputDebugString(_T("EVENT_CONSOLE_UPDATE_SIMPLE\n"));
+    {
+        TCHAR output[1024];
+
+        COORD coord;
+        coord.X = LOWORD(idObject);
+        coord.Y = HIWORD(idObject);
+
+        SHORT chUpdate = LOWORD(idChild);
+        SHORT wAttributes = HIWORD(idChild);
+
+        _stprintf_s(output, 1024, _T("EVENT_CONSOLE_UPDATE_SIMPLE: [X: %d, X: %d, Char: %d, Attr: %d]\n"), coord.X, coord.Y, chUpdate, wAttributes);
+        OutputDebugString(output);
         break;
+    }
     case EVENT_CONSOLE_UPDATE_SCROLL:
-        OutputDebugString(_T("EVENT_CONSOLE_UPDATE_SCROLL\n"));
+    {
+        TCHAR output[1024];
+        _stprintf_s(output, 1024, _T("EVENT_CONSOLE_UPDATE_SCROLL: [dx: %o, dy: %o]\n"), idObject, idChild);
+        OutputDebugString(output);
         break;
+    }
     case EVENT_CONSOLE_LAYOUT:
         OutputDebugString(_T("EVENT_CONSOLE_LAYOUT\n"));
         break;
     case EVENT_CONSOLE_START_APPLICATION:
-        OutputDebugString(_T("EVENT_CONSOLE_START_APPLICATION\n"));
+    {
+        TCHAR output[1024];
+        _stprintf_s(output, 1024, _T("EVENT_CONSOLE_START_APPLICATION: [Process ID: %o]\n"), idObject);
+        OutputDebugString(output);
         break;
+    }
     case EVENT_CONSOLE_END_APPLICATION:
-        OutputDebugString(_T("EVENT_CONSOLE_END_APPLICATION\n"));
+    {
+        TCHAR output[1024];
+        _stprintf_s(output, 1024, _T("EVENT_CONSOLE_END_APPLICATION: [Process ID: %o]\n"), idObject);
+        OutputDebugString(output);
         break;
+    }
     default:
         OutputDebugString(_T("UNKNOWN EVENT\n"));
         break;
@@ -64,22 +123,22 @@ int main()
         return 1;
     }
 
-    hInputConsole = GetStdHandle(STD_INPUT_HANDLE);
-    hOutputConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    g_hInputConsole = GetStdHandle(STD_INPUT_HANDLE);
+    g_hOutputConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    if (hInputConsole == INVALID_HANDLE_VALUE || hInputConsole == NULL || hOutputConsole == INVALID_HANDLE_VALUE || hOutputConsole == NULL) {
+    if (g_hInputConsole == INVALID_HANDLE_VALUE || g_hInputConsole == NULL || g_hOutputConsole == INVALID_HANDLE_VALUE || g_hOutputConsole == NULL) {
         printLastError(_T("GetStdHandle failed with %d\n"));
         return 1;
     }
 
     SMALL_RECT windowRect = { 0, 0, CONSOLE_WIDTH - 1, CONSOLE_HEIGHT - 1 };
-    if (!SetConsoleWindowInfo(hOutputConsole, TRUE, &windowRect)) {
+    if (!SetConsoleWindowInfo(g_hOutputConsole, TRUE, &windowRect)) {
         printLastError(_T("SetConsoleWindowInfo failed with %d\n"));
         return 1;
     }
 
     COORD bufferSize = { CONSOLE_WIDTH, CONSOLE_HEIGHT };
-    if (!SetConsoleScreenBufferSize(hOutputConsole, bufferSize)) {
+    if (!SetConsoleScreenBufferSize(g_hOutputConsole, bufferSize)) {
         printLastError(_T("SetConsoleScreenBufferSize failed with %d\n"));
         return 1;
     }
